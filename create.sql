ae0ef1ee9e1c2819ec580a71ef4b0c69b8326eee
rocha19 <rochaarqueologia@outlook.com>
2024-06-21 13:30:17 -0300

    feat: add and use sqlite

---
+++ create.sql
@@ -0,0 +1,46 @@
+-- SQLITE
+
+-- Não há suporte para SCHEMA no SQLite, então removemos DROP SCHEMA e CREATE SCHEMA
+-- Também não há suporte para a palavra reservada UUID no SQLite, então removemos o PRIMARY KEY
+CREATE TABLE event (
+  event_id TEXT PRIMARY KEY,
+  description TEXT,
+  price NUMERIC
+);
+
+CREATE TABLE ticket (
+  ticket_id TEXT PRIMARY KEY,
+  event_id TEXT,
+  email TEXT,
+  price NUMERIC,
+  FOREIGN KEY(event_id) REFERENCES event(event_id)
+);
+
+INSERT INTO event (
+  event_id, description, price
+) VALUES ( '161d4eea-cc10-4c42-94d6-5a09fb3bd72e', 'Rockfest', 100.00 );
+
+INSERT INTO ticket (
+  ticket_id, event_id, email, price
+) VALUES ( 'dc819810-3fd2-4db3-a4bc-dc53619b2c8c', '161d4eea-cc10-4c42-94d6-5a09fb3bd72e', 'any@email.com', 100.00 );
+
+-- POSTGRES
+
+-- DROP SCHEMA IF EXISTS rocha CASCADE;
+-- CREATE SCHEMA rocha;
+
+-- CREATE TABLE rocha.event (
+--   event_id UUID PRIMARY KEY,
+--   description TEXT,
+--   price NUMERIC
+-- );
+--
+-- CREATE TABLE rocha.ticket (
+--   ticket_id UUID PRIMARY KEY,
+--   event_id UUID REFERENCES rocha.event(event_id)
+-- );
+--
+-- INSERT INTO rocha.event (
+--   event_id, description, price
+-- ) VALUES ( '161d4eea-cc10-4c42-94d6-5a09fb3bd72e', 'Rockfest', 100.00 );
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+extern crate rusqlite;
+
+use rusqlite::{Connection, Result};
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> Result<()> {
+    let mut file = File::open("create.sql").expect("Não foi possível abrir o arquivo create.sql");
+    let mut sql_script = String::new();
+    file.read_to_string(&mut sql_script)
+        .expect("Não foi possível ler o arquivo create.sql");
+
+    let conn = Connection::open("database.sqlite")?;
+
+    conn.execute_batch(&sql_script)?;
+
+    println!("Banco de dados SQLite criado com sucesso!");
+
+    Ok(())
 }
